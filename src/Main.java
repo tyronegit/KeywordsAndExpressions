414521e0438f6aa77ccfc901ece5e2e7d1403e31
T.Atkins <43272999+tyronegit@users.noreply.github.com>
2024-03-12 21:41:56 -0400

    Initial commit new revisit of Java

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+
+    public static void main(String[] arg){
+//        // A valid Java statement consisting of operators, datatype,and values.
+//        double kilometers = (100 * 1.609344);
+//
+//        int highScore = 50;
+//
+//        if (highScore > 25) {
+//            highScore = 1000 + highScore; // add bonus points
+//        }
+//
+//        int health = 100; // expressions 6
+//
+//        if ((health < 25) && (highScore > 1000)) {
+//            highScore = highScore - 1000;
+//
+//        }
+        // valid statement
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+
+        System.out.println("This is a test");
+
+        System.out.println("This is" +
+                " another" +
+                " still more.");
+
+
+        int anotherVariable = 50;
+        myVariable--;
+        System.out.println("myVariable = " + myVariable);
+        System.out.println("anotherVariable = " + anotherVariable);
+     }
+
+}
